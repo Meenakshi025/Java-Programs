69f9a81749e24f9a487d50589a4c5087836112bb
Meenakshi <meenakshi.patil025@gmail.com>
2021-04-17 15:09:06 +0530

    Java Concepts

---
+++ JavaQuestions/SwapNumbers.java
@@ -0,0 +1,47 @@
+package JavaQuestions;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		
+//		System.out.println("before swapping:");
+//		System.out.println("a="+a);
+//		System.out.println("b="+b);
+//		
+//		System.out.println("after swapping:");
+//
+//		int temp;
+//		temp = a;
+//		a = b;
+//		b = temp;
+//		
+//		System.out.println("a="+a);
+//		System.out.println("b="+b);
+		
+		//without temp var:
+		System.out.println("before swapping:");
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+
+		System.out.println("after swapping:");
+
+//		a = a-b;//-10
+//		b = a+b;//10
+//		a = b-a;//20
+//		
+		
+		a = a+b;//-10
+		b = a-b;//10
+		a = a-b;//20
+		
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+
+		
+		
+	}
+
+}
