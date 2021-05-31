07aab919a3a78f3f3e4de8950ef6c9dae53376cd
Meenakshi <meenakshi.patil025@gmail.com>
2021-05-31 16:06:59 +0530

    Adding java programs

---
+++ JavaPrograms/isUnique.java
@@ -0,0 +1,25 @@
+package JavaPrograms;
+
+public class isUnique {
+	
+	public static void main(String args[]) {
+		
+		int count = 0;
+		String s = "patil";
+		char a[] = s.toCharArray();
+		
+		for(int i=0; i<=a.length-1; i++) {
+			if (s.indexOf(a[i]) != s.lastIndexOf(a[i])) {
+				count++;
+				}
+		}
+		
+		if (count >=1) {
+			System.out.println("The String has duplicate characters.");
+		}
+		
+		else {
+			System.out.println("The String has all unique characters.");
+		}
+}
+}
